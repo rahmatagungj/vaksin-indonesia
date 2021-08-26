b9d0d65d654fce27dc98f6da27acf6706b0be7f6
Rahmat Agung Julians <rahmatagungj@Gmail.com>
2021-08-26 09:23:24 +0700

    first init

---
+++ src/js/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "react-dom";
+import "../scss/App.scss";
+
+const App = () => {
+  return (
+    <div>
+      <h1>hai</h1>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById("root"));
